4fe4bb15d5a54aa8abf00321e34035b5d9797734
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2024-03-05 23:40:39 +0100

    form validaion part 1

--- src/App/functions.php
+++ src/App/functions.php
@@ -9,6 +9,15 @@ function dd(mixed $value)
     echo "</pre>";
     die();
 }
-function esc(mixed $value) {
+
+function esc(mixed $value)
+{
     return htmlspecialchars((string) $value);
+}
+
+function redirectTo(string $path)
+{
+    header("Location: {$path}");
+    http_response_code(302);
+    exit;
 }
--- src/Framework/Contracts/RuleInterface.php
+++ src/Framework/Contracts/RuleInterface.php
@@ -4,10 +4,9 @@ declare(strict_types=1);
 
 namespace Framework\Contracts;
 
-class RuleINterface
+interface RuleInterface
 {
-    public function validate()
-    {
-        
-    }
+    public function validate(array $data, string $field, array $params): bool;
+
+    public function getMessage(array $data, string $field, array $params): string;
 }
