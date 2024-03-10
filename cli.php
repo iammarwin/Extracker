f973cba6c9fd9d064e97f5e180c2323e53321805
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2024-03-10 12:40:09 +0000

    creating users table and loading files

--- cli.php
+++ cli.php
@@ -10,13 +10,31 @@ $db = new Database('mysql', [
     'dbname' => 'extracker_db'
 ], 'root', '');
 
-$search = "Sweaters";
-$query = "SELECT * FROM products WHERE name = :name";
 
-$stmt = $db->connection->prepare($query);
+// try {
+//     $db->connection->beginTransaction();
+//     $db->connection->query("INSERT INTO products VALUES(99, 'Shoes')");
 
-$stmt->bindValue('name', $search, PDO::PARAM_STR);
+//     $search = "Sweaters";
+//     $query = "SELECT * FROM products WHERE name = :name";
 
-$stmt->execute();
+//     $stmt = $db->connection->prepare($query);
 
-var_dump($stmt->fetchAll(PDO::FETCH_ASSOC));
+//     $stmt->bindValue('name', 'Shoes', PDO::PARAM_STR);
+
+//     $stmt->execute();
+
+//     var_dump($stmt->fetchAll(PDO::FETCH_ASSOC));
+
+//     $db->connection->commit();
+// } catch (Exception $e) {
+//     if ($db->connection->inTransaction()) {
+//         $db->connection->rollBack();
+//     }
+//     echo "Transaction Failed.";
+// }
+
+
+$sqlFile = file_get_contents("./database.sql");
+
+$db->connection->query($sqlFile);
